4b74913b209c8a64bf67f638bc16ce72b201f113
Yauheniya Nikulyak <ynikulyak@csumb.edu>
2020-03-30 16:26:48 -0700

    Initial commit for Frontend

---
+++ src/main/java/cst438/Team11ProjectFrontEndApplication.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team11ProjectFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team11ProjectFrontEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cst438/Team11ProjectFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team11ProjectFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
