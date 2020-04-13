cef48d0285bef3f1b15020dbb2f35b01dc49923c
Yauheniya Nikulyak <ynikulyak@csumb.edu>
2020-04-13 16:17:21 -0700

    Adds search result page which loads search results with AJAX returned via Forwarding Service from Common Flight Information service

--- src/main/java/cst438/controller/CommonFlightServiceForwardingController.java
+++ src/main/java/cst438/controller/CommonFlightServiceForwardingController.java
@@ -3,6 +3,7 @@ package cst438.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import cst438.domain.Airport;
+import cst438.domain.Flight;
 import cst438.service.CommonFlightForwardingService;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -26,5 +28,16 @@ public class CommonFlightServiceForwardingController {
    @GetMapping("/commonFlightService/airport/{id}")
    public Airport getRent(@PathVariable("id") String id) {
       return commonFlightService.getAirport(id);
+   }
+   
+   @GetMapping("/commonFlightService/flight/{code}")
+   public Flight getFlight(@PathVariable("code") String code) {
+      return commonFlightService.getFlight(code);
+   }
+   
+   @GetMapping("/commonFlightService/flights/{from}/{to}")
+   public List<Flight> getFlight(@PathVariable("from") String from, @PathVariable("to") String to, 
+         @RequestParam("departure") String departureDate, @RequestParam("return") String returnDate){
+      return commonFlightService.getFlights(from, to, departureDate, returnDate);
    }
 }
--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -58,19 +58,21 @@ function validateAndSubmit() {
 	var now = new Date();
 	var departureDate = $("#departure").val().trim();
 	var returnDate = $("#return").val().trim();
-	var monthNumber = now.getMonth()+1;
-	monthNumber = monthNumber < 10 ? '0'+ monthNumber : monthNumber+'';
+	var monthNumber = now.getMonth() + 1;
+	monthNumber = monthNumber < 10 ? '0' + monthNumber : monthNumber + '';
 	var todayDate = now.getFullYear() + '-' + monthNumber + '-' + now.getDate();
-	
-	if (departureDate.length === 0 || returnDate.length === 0 || todayDate > departureDate || departureDate > returnDate){
+
+	if (departureDate.length === 0 || returnDate.length === 0
+			|| todayDate > departureDate || departureDate > returnDate) {
 		valid = false;
 		console.log("wrong date");
-		console.log("departureDate / todayDate / returnDate: " + departureDate + '/' + todayDate + '/' + returnDate);
+		console.log("departureDate / todayDate / returnDate: " + departureDate
+				+ '/' + todayDate + '/' + returnDate);
 		$("#departure").addClass("error");
 		$("#return").addClass("error");
 		$("#invalidDeparture").show();
 		$("#invalidReturn").show();
-	}else {
+	} else {
 		$("#departure").removeClass("error");
 		$("#return").removeClass("error");
 		$("#invalidDeparture").hide();
@@ -85,7 +87,7 @@ function validateAndSubmit() {
 		console.log("wrong FromAirprot");
 		$("#inputFrom").addClass("error");
 		$("#invalidFrom").show();
-	}else {
+	} else {
 		$("#inputFrom").removeClass("error");
 		$("#invalidFrom").hide();
 	}
@@ -95,7 +97,7 @@ function validateAndSubmit() {
 		console.log("invalid toAirport");
 		$("#inputTo").addClass("error");
 		$("#invalidTo").show();
-	}else {
+	} else {
 		$("#inputTo").removeClass("error");
 		$("#invalidTo").hide();
 	}
@@ -107,7 +109,7 @@ function validateAndSubmit() {
 	}
 }
 
-$(document).ready(function() {
+function initializeIndexPage() {
 	hideCalendar();
 	$(".invalid-feedback").hide();
 
@@ -135,4 +137,69 @@ $(document).ready(function() {
 			$("#inputToId").val(suggestion.item.airportId);
 		}
 	});
-});
+}
+
+function searchFlight(from, to, departure, returnDate) {
+	var number = 0;
+	$.ajax({
+		method : "get",
+		url : "/commonFlightService/flights/" + encodeURIComponent(from) + '/'
+				+ encodeURIComponent(to) + "?departure="
+				+ encodeURIComponent(departure) + "&return="
+				+ encodeURIComponent(returnDate),
+		dataType : "json",
+		success : function(data) {
+			$(".list-group").empty();
+			$(".card-text").append()
+			var prevFrom = null;
+			$.each(data, function(index, element) {
+				prevFrom = prevFrom || element.fromAirportCode;
+				number = number + 1;
+				$(".list-group").append(
+						createEntryForElement(element, prevFrom));
+				prevFrom = element.fromAirportCode;
+			});
+		}
+	});
+	return false;
+}
+
+function createEntryForElement(element, prevFrom) {
+	var startLi = "<li class='list-group-item'>";
+	var html = '';
+	if (prevFrom != element.fromAirportCode) {
+		html += startLi + '<h3>Returning flights</h3></li>';
+	}
+
+	html += startLi + "<b>FLIGHT:</b> <span class='flightcode'>"
+			+ element.flightCode + "</span><br /> " + "<b>FROM: </b>"
+			+ element.airportTitleFrom + " (<b class='airportcode'>"
+			+ element.fromAirportCode + "</b>) <br/>" + "<b>TO: </b>"
+			+ element.airportTitleTo + " (<b class='airportcode'>"
+			+ element.toAirportCode + "</b>) <br/>"
+			+ "<span class='details'><b>Departure time: </b>"
+			+ element.departure + "</span> "
+			+ "<span class='details'><b>Arrivale time: </b>" + element.arrival
+			+ "</span>" + "</li>";
+	return html;
+}
+
+function startSearchingFlights() {
+	searchFlight(getUrlParameter("from"), getUrlParameter("to"),
+			getUrlParameter("departure"), getUrlParameter("return"));
+}
+
+// Returns GET variable from the current URL in browser.
+function getUrlParameter(sParam) {
+	var sPageURL = window.location.search.substring(1), sURLVariables = sPageURL
+			.split('&'), sParameterName, i;
+
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=');
+
+		if (sParameterName[0] === sParam) {
+			return sParameterName[1] === undefined ? true
+					: decodeURIComponent(sParameterName[1]);
+		}
+	}
+}
